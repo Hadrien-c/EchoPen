562233eda0e6cc49ad7719d13ced61d4a22f4e3f
Hadrien-c <hadrien.courapied@hetic.net>
2017-07-05 10:40:04 +0200

    Init sass

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
-
-	
+	// VOLET
+	var menuBtn = $('.menu_btn');
+	var menu = $('.menu_list');
+	console.log(menu);
+	menuBtn.on('click', function(){
+		menu.toggleClass('menu_open');
+		console.log('menu open');
+		// var hauteur = $('.menu_list').prop('scrollHeight');
+		// $('.menu_open').css("max-height", hauteur);
+	});
 });
